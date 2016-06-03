919f883aa411a7a9a86ca3a47185cb7473d40870
hmedal <hugh.medal@gmail.com>
2016-06-03 11:36:48 -0500

    Added some more text for planning.

--- src/master/execute.py
+++ src/master/execute.py
@@ -1,11 +1,13 @@
 
-# python execute -n networkFile -i interferenceModelAndInfo -e experimentFile
+# python execute -n networkFile (optional) -i interferenceModelAndInfo (optional) -e experimentFile -d demandFile (optional) -f jammingDefFile (optional)
 
 # read in parameters from command line
 
-# read in experiment file
+# read in experiment file using XML
 
-# run david's script
+# run david's script -n networkFile -i (optional) interferenceModelAndInfo (optional)
+
+# run jamming generator script -f jammingDefFile
 
 # run the math model, print some results to file, print some results to database
 
